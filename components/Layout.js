0d08aa54814407b77c62c1cb9c916fafa1c2aab9
Paige Niedringhaus <paigen11@gmail.com>
2020-11-13 16:49:33 -0500

    first commit

---
+++ components/Layout.js
@@ -0,0 +1,30 @@
+import Head from 'next/head';
+import Link from 'next/link';
+
+const Layout = ({ children }) => {
+  return (
+    <div>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <nav>
+        <ul>
+          <li>
+            <Link href="/">
+              <a>Home</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/about">
+              <a>About</a>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <main>{children}</main>
+    </div>
+  );
+};
+
+export default Layout;
