45dba0df6b417b46f868cd2cb1931d0a8e90b641
Paige Niedringhaus <paigen11@gmail.com>
2021-02-09 16:59:44 -0500

    commit for the day

--- components/HabitForm.js
+++ components/HabitForm.js
@@ -1,11 +1,32 @@
 import { Form, Field } from '@leveluptuts/fresh';
+import { useMutation } from '@apollo/react-hooks';
+import gql from 'graphql-tag';
 
-const HabitForm = ({ setHabits }) => {
+const ADD_HABIT = gql`
+  mutation addHabit($habit: HabitInput) {
+    addHabit(habit: $habit) {
+      _id
+      name
+    }
+  }
+`;
+
+const HabitForm = () => {
+  const [addHabit] = useMutation(ADD_HABIT, {
+    refetchQueries: ['getHabits'],
+  });
+
   return (
     <Form
       onSubmit={(data) => {
-        console.log(data);
-        setHabits((prevState) => [...prevState, data.habit]);
+        // how to pass data to a graphql query
+        addHabit({
+          variables: {
+            habit: {
+              name: data.habit,
+            },
+          },
+        });
       }}
     >
       <Field>Habit</Field>
