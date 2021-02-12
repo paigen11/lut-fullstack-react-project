6ce3d56e89d08ad890ede04e87f8a6abdedd654b
Paige Niedringhaus <paigen11@gmail.com>
2021-02-12 17:37:52 -0500

    habit button commit

--- components/HabitButton.js
+++ components/HabitButton.js
@@ -1,4 +1,32 @@
+import { useMutation } from '@apollo/react-hooks';
 import { useState } from 'react';
+import gql from 'graphql-tag';
+
+const ADD_EVENT = gql`
+  mutation addEvent($date: Date, $habitId: ID) {
+    addEvent(date: $date, habitId: $habitId) {
+      _id
+      name
+      events {
+        _id
+        date
+      }
+    }
+  }
+`;
+
+const REMOVE_EVENT = gql`
+  mutation removeEvent($eventId: ID, $habitId: ID) {
+    removeEvent(eventId: $eventId, habitId: $habitId) {
+      _id
+      name
+      events {
+        _id
+        date
+      }
+    }
+  }
+`;
 
 const HabitButton = ({ date }) => {
   const [complete, setComplete] = useState(false);
