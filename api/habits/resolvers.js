e92c964db1290f53dd9c93ae268fc03667ba6180
Paige Niedringhaus <paigen11@gmail.com>
2021-02-15 09:09:26 -0500

    adding resolvers

--- api/habits/resolvers.js
+++ api/habits/resolvers.js
@@ -1,13 +1,33 @@
+import { GraphQLScalarType } from 'graphql';
+import { Kind } from 'graphql/language';
+import Habits from './habits';
+
 export const habitsResolvers = {
   Query: {
     async habits() {
-      console.log('habits');
-      return [
-        {
-          _id: 'somefunkyarray',
-          name: 'make my bed',
-        },
-      ];
+      try {
+        const habits = await Habits.find();
+        return habits;
+      } catch (err) {
+        console.log(err);
+      }
     },
   },
+
+  Date: new GraphQLScalarType({
+    name: 'Date',
+    description: 'Date custom scalar',
+    parseValue(value) {
+      return new Date(value); // value from the client
+    },
+    serialize(value) {
+      return value.getTime(); //value sent to the client
+    },
+    parseLiteral(ast) {
+      if (ast.kind === Kind.INT) {
+        return new Date(ast.value);
+      }
+      return null;
+    },
+  }),
 };
